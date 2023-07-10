694230e187483f428ea5c3eaf8af740cec621674
1uhis <106042973+1uhis@users.noreply.github.com>
2023-07-10 14:52:40 +1200

    Add files via upload

---
+++ frontend/src/Homepage/home.js
@@ -0,0 +1,16 @@
+// components/HomePage.js
+import React from 'react';
+import Navbar from '../components/navbar';
+
+function HomePage() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to the Home Page</h1>
+      {/* Add your homepage content here */}
+    </div>
+  );
+}
+
+export default HomePage;
+
